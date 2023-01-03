e694809902e4688c5de8aec18a34c02f6d81dd80
Wilfred Erdo <wilfred05777@gmail.com>
2023-01-03 10:03:01 +0800

    29. Understanding Requests

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 // function rqListener(req, res) {}
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
   // process.exit();
 });
 
