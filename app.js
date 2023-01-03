56d41f308e18ef4e7bf80d3c5164bd27965b28b1
Wilfred Erdo <wilfred05777@gmail.com>
2023-01-03 10:31:20 +0800

    32. Routing Requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 // function rqListener(req, res) {}
 
@@ -7,6 +8,7 @@ const server = http.createServer((req, res) => {
   // process.exit();
 
   const url = req.url;
+  const method = req.method;
   /// if the visited url is / in the root
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
@@ -18,6 +20,17 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
+  if (url === "/message" && method === "POST") {
+    // fs.writeFile("message.txt", "DUMMY");
+    fs.writeFileSync("message.txt", "DUMMY");
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+    // res.writeHead(302, { });
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
