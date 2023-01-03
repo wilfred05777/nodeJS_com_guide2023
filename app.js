bdcd8d176e64233b39759df9f4ebd36f5ecc0836
Wilfred Erdo <wilfred05777@gmail.com>
2023-01-03 10:23:55 +0800

    32. Routing Requests

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const http = require("http");
 // function rqListener(req, res) {}
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
   // process.exit();
 
+  const url = req.url;
+  /// if the visited url is / in the root
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server</h1></body>");
   res.write("</html>");
-
   res.end();
 });
 
