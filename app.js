ef2cd8a581e72a60be8111afda9c477e5daa5b72
Wilfred <wilfred05777@gmail.com>
2023-01-05 15:15:44 +0800

    65. Limiting Middleware Execution to POST Requests

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.use("/add-product", (req, res, next) => {
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button> </form>'
   );
 });
-app.use("/product", (req, res, next) => {
+
+// 65. Limiting Middleware Execution to POST Requests
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
   // console.log("In another middleware");
-  res.send("<h1>Add Product Page </h1>");
+  res.send("<h1>Hello from Express! </h1>");
 });
 
 app.listen(3000);
