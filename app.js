959b8c62cf2c5f7d2db1a7276fd9e9674d527989
Wilfred <wilfred05777@gmail.com>
2023-01-05 13:36:57 +0800

    60. Adding Middleware - next() to go to next middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //// Allows the request to continue to the next middleware in line || without next it will go to the other middleware, ||
+});
+
+/// Without the next() it will not read this middleware
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
