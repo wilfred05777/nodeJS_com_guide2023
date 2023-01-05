0b8434e1fda05d5df1c105619d717267acd72104
Wilfred <wilfred05777@gmail.com>
2023-01-06 04:27:00 +0800

    70. Serving HTML Pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 /// default route must be on the bottom
 router.get("/", (req, res, next) => {
-  res.send(`<h1>Hello from express</h1>`);
+  // res.sendFile("/views/shop.html"); // error
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
