0b079b6b4aa8b5df78e541841bbf69e17f9dd8fd
Wilfred <wilfred05777@gmail.com>
2023-01-06 15:46:40 +0800

    79. Sharing Data Across Requests & Users - wala kaayo kasabot

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 /// import Routes
-const adminRoutes = require("./routes/admin");
+// const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const profileRoutes = require("./routes/profile");
 const listRoutes = require("./routes/listview");
@@ -16,15 +17,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(listRoutes);
 app.use(profileRoutes);
 // 68. Filtering Paths
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes); /// default routes
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html")); // this also works
-  //res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html")); /// working
-  // res.status(404).send(`
-  //   <h1>Page not found!</h1>
-  // `);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,13 @@ const express = require("express");
 
 const rootDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 /// default route must be on the bottom
 router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
   // res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
   // res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
