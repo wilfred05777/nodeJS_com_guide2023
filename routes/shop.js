232e53a060dbd635bac809835b7a1fc055a6ffa2
Wilfred <wilfred05777@gmail.com>
2023-01-06 17:16:32 +0800

    82. Outputting Dynamic Content - intro in pug

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,10 @@ const router = express.Router();
 
 /// default route must be on the bottom
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+
+  res.render("shop", { prods: products, docTitle: "Shop Pugs" });
+
   // console.log("shop.js", adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
